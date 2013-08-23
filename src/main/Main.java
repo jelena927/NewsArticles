10ff7715bfda2ad38f164aa80c2ac7a2e5b5c2a1
jelena927 <jelena927@gmail.com>
2013-08-24 00:05:10 +0200

    commit

---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+
+import parser.NewsArticleParser;
+import persistance.RDFModel;
+
+import domain.NewsArticle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			NewsArticle article = NewsArticleParser.parse("http://travel.nytimes.com/2013/08/11/travel/36-hours-in-lecce-italy.html");
+			//System.err.println(article);
+			RDFModel.getInstance().save(article);
+			RDFModel.getInstance().printOut();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/persistance/RDFModel.java
@@ -0,0 +1,49 @@
+package persistance;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import thewebsemantic.Bean2RDF;
+import thewebsemantic.RDF2Bean;
+import util.Constants;
+
+public class RDFModel {
+
+	Model graph;
+	Bean2RDF writer;
+	RDF2Bean reader;
+	
+	private static RDFModel INSTANCE;
+	
+	private RDFModel (){
+		graph = ModelFactory.createDefaultModel();
+		graph.setNsPrefix("NewsArticle", Constants.NS);
+		graph.setNsPrefix("schema", Constants.SCHEMA);
+		graph.setNsPrefix("xsd", Constants.XSD);
+		
+		
+		writer = new Bean2RDF(graph);
+		reader = new RDF2Bean(graph);
+	}
+	
+	public static RDFModel getInstance(){
+		if (INSTANCE == null) {
+			INSTANCE = new RDFModel();
+		}
+		
+		return INSTANCE;
+	}
+	
+	public void save(Object o) {
+		writer.save(o);
+	}
+	
+	
+	public Object load(String uri) {
+		return reader.load(uri);
+	}
+	
+	public void printOut(){
+		graph.write(System.out, "RDF/XML");
+	}
+}
