80f01b9de377506f2dfeaaaa1f8e88107f95a8c0
jelena927 <jelena927@gmail.com>
2013-08-27 20:52:52 +0200

    fix

--- src/util/URIGenerator.java
+++ src/util/URIGenerator.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public class URIGenerator {
 
 	public static <T extends domain.Thing> URI generate(T resource) throws URISyntaxException{
-		String uri = Constants.SCHEMA + 
+		String uri = Constants.NS + 
 				resource.getClass().getSimpleName() + 
 				"/" + UUID.randomUUID();
 		return new URI(uri);
